b115c2d5a0de067651bcdbc5f424b7020b8d2dca
Barbara <nikolaevavarvara95@gmail.com>
2026-02-24 16:00:38 +0300

    Add files via upload

---
+++ projects_2/task_2_7/task_2-7_1.py
@@ -0,0 +1,7 @@
+files = ["seq1", "seq2", "seq3", "seq4"]
+
+for name in files:
+
+   new_name = name + ".fasta" + " (Дата взятия образца: 24.02.2026)"
+
+   print(f"{new_name}")
---
+++ projects_2/task_2_7/task_2-7_3.py
@@ -0,0 +1,7 @@
+seq = [ "ATATACGCGTA", "CTTCGGNGGA" ]
+print(f"{seq}")
+
+for element in seq:
+    print(f"{element}\n")
+
+print("Цикл выполнен")
